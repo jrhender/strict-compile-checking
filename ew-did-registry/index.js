a6dc3a5df444fa6e3dad0eb84a2a7b330359d6b0
John Henderson <john.henderson@energyweb.org>
2021-03-05 15:41:38 -0800

    initial commit of ew-did-registry. no compilation errors

---
+++ ew-did-registry/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var didregistry = require("@ew-did-registry/did-registry");
+var didethrresolver = require("@ew-did-registry/did-ethr-resolver");
+var didethrinterface = require("@ew-did-registry/did-resolver-interface");
+var d = didregistry["default"];
+var e = didethrresolver.defaultProvider;
+var i = didethrinterface.Algorithms;
---
+++ ew-did-registry/index.ts
@@ -0,0 +1,7 @@
+import * as didregistry from "@ew-did-registry/did-registry"
+import * as didethrresolver from "@ew-did-registry/did-ethr-resolver"
+import * as didethrinterface from "@ew-did-registry/did-resolver-interface"
+
+const d = didregistry.default;
+const e = didethrresolver.defaultProvider;
+const i = didethrinterface.Algorithms;
